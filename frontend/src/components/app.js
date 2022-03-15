91f044bfff26a0ca410e5ea8db76f87f1c2065e5
egumin <ethangumin@gmail.com>
2022-03-14 21:00:46 -0400

    add Roboto font to index.html and begin formatting sign up form

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
+import React from "react";
+import { Switch } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 import TopNavAuth from "./top_nav/top_nav_auth";
 import TopNavMain from "./top_nav/top_nav_main";
 import MainPage from "./main/main_page";
-import LoginFormContainer from './session/login_form_container';
-import SignupForm1Container from './session/signup_form_container';
+import LoginFormContainer from "./session/login_form_container";
+import SignupForm1Container from "./session/signup_form_container";
 
-import "./reset.css"
+import "./reset.css";
 
 const App = () => (
   <div>
@@ -24,7 +24,6 @@ const App = () => (
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} /> */}
     </Switch>
-
   </div>
 );
 
--- frontend/src/components/session/signup_form1.js
+++ frontend/src/components/session/signup_form1.js
@@ -1,99 +1,112 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { TextField, Button } from "@mui/material";
+
+
+import "./signup_form1.css";
 
 class SignupForm1 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            username: '',
-            password: '',
-            password2: '',
-            errors: {}
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      password2: "",
+      errors: {},
+    };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.clearedErrors = false;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearedErrors = false;
+  }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.signedIn === true) {
+      // to be changed once done with login modol
+      this.props.history.push("/login");
     }
+    // debugger;
+    this.setState({ errors: nextProps.errors });
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.signedIn === true) {
-            // to be changed once done with login modol
-            this.props.history.push('/login');
-        }
-        // debugger;
-        this.setState({ errors: nextProps.errors })
-    }
+  update(field) {
+    return (e) =>
+      this.setState({
+        [field]: e.currentTarget.value,
+      });
+  }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        });
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    let user = {
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+      password2: this.state.password2,
+    };
 
-    handleSubmit(e) {
-        e.preventDefault();
-        let user = {
-            email: this.state.email,
-            username: this.state.username,
-            password: this.state.password,
-            password2: this.state.password2
-        };
+    this.props.signup(user, this.props.history);
+  }
 
-        this.props.signup(user, this.props.history);
-    }
+  renderErrors() {
+    // debugger;
+    return (
+      <ul>
+        {Object.keys(this.state.errors).map((error, i) => (
+          <li key={`error-${i}`}>{this.state.errors[error]}</li>
+        ))}
+      </ul>
+    );
+  }
 
-    renderErrors() {
-        // debugger;
-        return (
-            <ul>
-                {Object.keys(this.state.errors).map((error, i) => (
-                    <li key={`error-${i}`}>
-                        {this.state.errors[error]}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
-
-    render() {
-        return (
-            <div className="signup-form-container">
-                <form onSubmit={this.handleSubmit}>
-                    <div className="signup-form">
-                        <br />
-                        <input type="text"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                            placeholder="Email"
-                        />
-                        <br />
-                        <input type="text"
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            placeholder="Username"
-                        />
-                        <br />
-                        <input type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            placeholder="Password"
-                        />
-                        <br />
-                        <input type="password"
-                            value={this.state.password2}
-                            onChange={this.update('password2')}
-                            placeholder="Confirm Password"
-                        />
-                        <br />
-                        <input type="submit" value="Submit" />
-                        {this.renderErrors()}
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="signup-form-container">
+        <h1>Let's get you signed up!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="signup-form">
+            <TextField
+              type="text"
+              value={this.state.email}
+              onChange={this.update("email")}
+              label="Email"
+              variant="outlined"
+            />
+            <TextField
+              type="text"
+              value={this.state.username}
+              onChange={this.update("username")}
+              label="Username"
+              variant="outlined"
+            />
+            <TextField
+              type="password"
+              value={this.state.password}
+              onChange={this.update("password")}
+              label="Password"
+              variant="outlined"
+            />
+            <TextField
+              type="password"
+              value={this.state.password2}
+              onChange={this.update("password2")}
+              label="Confirm Password"
+              variant="outlined"
+            />
+            {/* <input type="submit" value="Submit" /> */}
+            <Button type="submit" variant="contained">Submit</Button>
+            {this.renderErrors()}
+          </div>
+        </form>
+        <p>or sign in as a </p>
+        <Button variant="contained">Demo User</Button>
+        <p>Already have an account?</p>
+        <Link to="/login">
+          <Button variant="contained">Login Instead</Button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default withRouter(SignupForm1);
