b9071edd0c01b0e515cfccaaac8d67a15e2d7394
egumin <ethangumin@gmail.com>
2022-03-15 15:09:03 -0400

    basic user info rendered in UserShow container

--- frontend/src/components/users/user_show.jsx
+++ frontend/src/components/users/user_show.jsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-
+import React, { useEffect } from "react";
 import "./user_show.css";
 
 const UserShow = (props) => {
-  const [currUser, setCurrUser] = useState({
-    id: "",
-    username: "",
-    email: "",
-  });
+  useEffect(() => props.fetchUser(props.match.params.userId), []);
 
-  // useEffect(() => {
-  //   props.fetchUser(props.match.params.userId).then((user) => {
-  //     return setCurrUser(user.user);
-  //   });
-  // }, []);
+  const currUserInfo = props.currUser ? (
+    <div>{props.currUser.username}</div>
+  ) : (
+    <div></div>
+  )
 
   return (
     <div className="user-show__container">
-      <div></div>
+      <div>{currUserInfo}</div>
     </div>
   );
 };
