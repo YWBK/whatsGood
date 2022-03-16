ac30bb9ebbed1faf23fc671807984c7b5c86654f
egumin <ethangumin@gmail.com>
2022-03-16 16:26:12 -0400

    update items show link and render book in ItemsShow

--- frontend/src/components/items/items_show.jsx
+++ frontend/src/components/items/items_show.jsx
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from "react";
 import ItemsShowItem from "./items_show_item";
 import ItemsShowRatings from "./items_show_ratings";
 import { fetchSingleBook } from "../../util/search_util";
-import { useLocation } from "react-router-dom";
 
 const ItemsShow = (props) => {
   const [item, setItem] = useState(null);
 
-  const location = useLocation();
-  const { volumeId } = props.location.state;
-
-  //   debugger;
   useEffect(() => {
-    debugger;
-    fetchSingleBook(volumeId).then((book) => console.log(book));
+    // fetchSingleBook(volumeId).then((book) => console.log(book));
+    fetchSingleBook(props.match.params.itemId).then((book) => console.log(book));
   }, []);
 
   return (
--- frontend/src/components/lists/show/list_show_item.js
+++ frontend/src/components/lists/show/list_show_item.js
@@ -8,72 +8,76 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import BasicRating from "./rating";
+import BasicRating from "./rating"
+
 
 class ListShowItem extends React.Component {
-  constructor() {
-    super();
-    this.state = { book: null };
-  }
 
-  componentDidMount() {
-    const { volumeId } = this.props.book;
-    fetchSingleBook(volumeId).then((res) => {
-      console.log(res.data);
-      this.setState({ book: res.data });
-    });
-  }
+    constructor() {
+        super();
+        this.state = { book: null };
+    }
 
-  render() {
-    const { book } = this.props;
+    componentDidMount() {
+        const { volumeId } = this.props.book;
+        fetchSingleBook(volumeId).then(res => {
+            console.log(res.data);
+            this.setState({ book: res.data });
+        });
+    }
 
-    return (
-      <>
-        {this.state.book && (
-          <Link
-            to={{
-              pathname: `/items/${book._id}`,
-              state: { volumeId: this.props.book.volumeId },
-            }}
-            // target="_blank"
-          >
-            <AlignItemsList book={this.state.book.volumeInfo} />
-          </Link>
-        )}
-      </>
-    );
-  }
+    render() {
+        const { book } = this.props;
+        debugger;
+        return (
+            <>
+                {
+                    this.state.book && <Link
+                        to={`/items/${book.volumeId}`}
+                    // target="_blank"
+                    >
+                        <AlignItemsList book={this.state.book.volumeInfo} />
+                    </Link>
+                }
+            </>
+        );
+    }
 }
 export default ListShowItem;
 
 function AlignItemsList(props) {
-  return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Book1" src={`${props.book.imageLinks.thumbnail}`} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={`${props.book.title}`}
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                {`${props.book.subtitle}`}
-              </Typography>
-              <br />
-              {`by ${props.book.authors.map((author) => `${author} `)}`}
-            </React.Fragment>
-          }
-        />
-        <BasicRating book={props.book} />
-      </ListItem>
+    return (
+        <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar alt="Book1" src={`${props.book.imageLinks.thumbnail}`} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={`${props.book.title}`}
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                sx={{ display: "inline" }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                {`${props.book.subtitle}`}
+                            </Typography>
+                            <br />
+                            {`by ${props.book.authors.map(author => `${author} `)}`}
 
-      <Divider variant="inset" component="li" />
-    </List>
-  );
+                        </React.Fragment>
+                    }
+                />
+                <BasicRating book={props.book} />
+            </ListItem>
+
+            <Divider variant="inset" component="li" />
+
+        </List>
+    );
 }
+
+
+
