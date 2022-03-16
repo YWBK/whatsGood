8ae9cc97675ddac3992fbeb13cb584774b6cfe69
egumin <ethangumin@gmail.com>
2022-03-16 10:41:53 -0400

    user_show.jsx otherUserView renders

--- frontend/src/components/users/user_show_container.js
+++ frontend/src/components/users/user_show_container.js
@@ -6,10 +6,16 @@ const mSTP = (state, prevProps) => {
   // debugger;
   return {
     currentUser: state.entities.users[state.session.user.id],
-    user: state.entities.users[prevProps.match.params.userId],
-    userLists: Object.values(state.entities.lists.all).filter(
-      (list) => list.ownerId === prevProps.match.params.userId
-    ),
+    currentUserId: state.session.user.id,
+    // keep below
+    // user: state.entities.users[prevProps.match.params.userId],
+    // userLists: Object.values(state.entities.lists.all).filter(
+    //   (list) => list.ownerId === prevProps.match.params.userId
+    // ),
+    allLists: Object.values(state.entities.lists.all),
+
+    // temp
+    user: state.entities.users["623163debe6f6b61d32195c7"],
   };
 };
 
