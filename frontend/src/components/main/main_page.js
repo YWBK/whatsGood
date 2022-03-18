4b74dd6c96e264002210b13c2211fbdaf15aa4de
egumin <ethangumin@gmail.com>
2022-03-17 23:27:35 -0400

    working on splash

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
+import Button from "@mui/material/Button";
+import "./main_page.css";
 
 class MainPage extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <h1>whatsGood Main Page</h1>
-                <footer>
-                    Copyright &copy; 2022 whatsGood
-                </footer>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="splash__bg">
+        <div className="splash">
+          <h1>Find out whatsGood now!</h1>
+          <Button variant="contained">Login / Sign Up</Button>
+          <footer>Copyright &copy; 2022 whatsGood</footer>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
